859a8acdb53c39de04172e10be2d6868a710f5cd
Plaonder <39774970+FunGamez@users.noreply.github.com>
2021-04-27 21:49:06 -0700

    Did some troubleshooting
    Made stuff like customgamedirectory actually work now, and cleaned up the looks a bit.

--- starttools.bat
+++ starttools.bat
@@ -2,7 +2,6 @@
 echo This will startup the SteamVR Workshop tools without needing VR enabled. Enter your addon name and we're off!
 echo.
 echo If this script is unable to find the SteamVR folder everytime you run it,
-echo.
 echo Change customgamepath below in the code to your directory.
 echo.
 
@@ -11,9 +10,8 @@ SET customgamepath=
 
 SET /P_addonname= Please enter your addon name: 
 SET addonpath=content\steamtours_addons\
-
-echo Checking for the Steam Directory..
 echo.
+echo Checking for the Steam Directory..
 
 :: Steam directory check
 for /f "usebackq tokens=1,2,*" %%i in (`reg query "HKCU\Software\Valve\Steam" /v "SteamPath"`) do set "steampath=%%~k"
@@ -27,29 +25,40 @@ if not exist "%steampath%\steam.exe" (
 		)
 	) else set "steampath=%ProgramFiles(x86)%\steam"
 )
-
 IF exist "%steampath%" ( echo Found steam directory! Continuing... ) ELSE ( GOTO DontRunTools )
 
-echo.
-IF exist "%steampath%\steamapps\common\SteamVR\" ( SET "steamvr_dir=%steampath%\steamapps\common\SteamVR\" ) ELSE IF exist "%customgamepath%" ( SET "steamvr_dir=%steampath%\steamapps\common\SteamVR\" ) ELSE ( SET /P steamvr_dir=Couldn't find SteamVR tools! Please check you have SteamVR installed or specify the directory now:  )
+IF exist "%steampath%\steamapps\common\SteamVR\" (
+	SET "steamvr_dir=%steampath%\steamapps\common\SteamVR\"
+) ELSE ( 
+	if not exist "%customgamepath%" ( 
+		SET /P steamvr_dir=Couldn't find SteamVR tools! Please check you have SteamVR installed or specify the directory now: 
+		goto CheckForAddons
+	)
+	if exist "%customgamepath%\tools\" (
+		SET steamvr_dir=%customgamepath%
+		echo.
+		echo Custom directory found!
+		echo Using custom directory...
+		echo.
+		goto CheckForAddons
+	)
+)
 
+:CheckForAddons
 echo Checking if your addon exists...
-echo.
 if not exist "%steamvr_dir%\tools\steamvr_environments\content\steamtours_addons\%_addonname%" (
 	echo ERROR: Did not find the addon folder. Did you enter the addon name correctly/created the addon?
-	echo.
 	pause
 	goto End
 )
 if exist "%steamvr_dir%\tools\steamvr_environments\content\steamtours_addons\%_addonname%" (
 	echo Found the addon folder! Continuing...
-	echo.
-	goto :RunTools
+	goto RunTools
 )
 
 :RunTools
-echo Starting SteamVR Tools...
 echo.
+echo Starting SteamVR Tools...
 cd "%steamvr_dir%"
 set steamtourspath=tools\steamvr_environments\game\bin\win64\steamtours.exe
 echo Have fun! Can't wait to see what you create.
@@ -57,13 +66,13 @@ echo Have fun! Can't wait to see what you create.
 goto HaveFun
 
 :HaveFun
-echo Goodbye! Hope you had some fun mapping!
+echo Goodbye!
 pause
 goto End
 
 :DontRunTools
 echo Is Steam installed on this computer? Could not find the Steam directory.
-goto End
 pause
+goto End
 
 :End
